ffe0aa6e3d64d3c09c6998be236031958a1adb5f
Amila Madusanka <amilamad@gmail.com>
2018-05-21 22:13:32 +0530

    file read->convert->write added.  Still has some bugs

--- misc/csv_creator/main.js
+++ misc/csv_creator/main.js
@@ -2,15 +2,41 @@
 uts = require('../converters_js/unicode_to_singlish.js')
 fmtos = require('../converters_js/fmabaya_to_unicode.js')
 
+var line_no = 0;
 startConvertion();
 
 function startConvertion() {    
 	console.log('Converting started');
-	var inputText = "wkafkdkH";
+	var lineReader = require('readline').createInterface({
+	  input: require('fs').createReadStream('sentences.txt')
+	});
 
-	var unicodeText = fmtos.fmabayaToUnicode(inputText);
-	var outputText  = uts.unicodeToSinglish(unicodeText);      
+	lineReader.on('line', function (line) {
+		 var convertedLine = convert(line);
+		 console.log('Singlish text -> ' + convertedLine);
+		 
+		 writeLine(convertedLine);
+	});
+}
 
-	console.log(unicodeText + "\n");
-	console.log(outputText + "\n");
+function convert(text){
+	var unicodeText = fmtos.fmabayaToUnicode(text);
+	var outputText  = uts.unicodeToSinglish(unicodeText); 
+ 	return outputText;
+}
+
+
+function writeLine(text){
+	console.log("writeLine called");
+
+	var lineToWrite = (line_no++) + "|" + text + "\n";
+
+	var fs = require('fs');
+	fs.writeFile("out_put.csv", lineToWrite, function(err) {
+    	if(err) {
+       	  return console.log(err);
+   	}
+
+    	console.log("The file was saved!");
+}); 
 }
