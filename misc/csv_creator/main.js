50653da89f41b245b65dde1ed121736ebd1fbcd5
Amila Rathnayake <amila.rathnayake@simcentrictsl.local>
2018-05-24 23:32:34 +0530

    chapter 2 6 13 added and converted

--- misc/csv_creator/main.js
+++ misc/csv_creator/main.js
@@ -2,40 +2,6 @@
 uts = require('../converters_js/unicode_to_singlish.js')
 fmtos = require('../converters_js/fmabaya_to_unicode.js')
 
-convertChapter("001");
-
-function convertChapter(chapterID) {   
-	var source_data_dir			= "input_files/";
-	var destination_data_dir	= "output_files/";
-	var input_file_name 		= source_data_dir + chapterID + ".txt";
-	var output_file_name		= destination_data_dir + chapterID + ".csv"
-
-	console.log('Converting started');
-	fs = require('fs');
-	fs.readFile(input_file_name, 'utf8', function (err,data) {
-			if (err) {
-				return console.log(err);
-			}
-			
-			var os = require('os');
-			var lines = data.split(os.EOL);
-
-			var output_data = "";	  	
-			var sentence_id = 1;
-			
-			for (var i = 0; i < lines.length; i++) { 
-				if(lines[i] != ""){
-				var convertedLine = "SIN" + chapterID + "-" + generateSentenceID(sentence_id, 4) + "|" + convert(lines[i]) + "\n";
-				output_data += convertedLine;	
-
-				sentence_id++; 
-				}    
-			}	
-
-			flushToFile(output_data, output_file_name);
-	});
-}
-
 function replaceAll(target, search, replacement) {
     return target.split(search).join(replacement);
 }
@@ -54,11 +20,10 @@ function convert(text){
  	return outputText;
 }
 
-function flushToFile(text, file_name)
-{
-	var fs = require('fs');
-	fs.writeFileSync(file_name, text);
- 	console.log("The file was saved!"); 	
+function cleanFMAbaya(text){
+	var regex = new RegExp("^{[0-9]*}.", "g");
+	var replaced = text.search(regex);
+	return retText;
 }
 
 function generateSentenceID(number, noSize){
@@ -78,5 +43,51 @@ function generateSentenceID(number, noSize){
 
 	return prefix + String(number);
 }
+
+function flushToFile(text, file_name)
+{
+	var fs = require('fs');
+	fs.writeFileSync(file_name, text);
+	console.log('Files saved ->' + file_name);
+}
+
+//convertFile("002");
+//convertFile("006");
+convertFile("013");
+//cleanFMAbaya("111abc");
+
+function convertFile(chapterID) {   
+	var source_data_dir			= "input_files/";
+	var destination_data_dir	= "output_files/";
+	var input_file_name 		= source_data_dir + chapterID + ".txt";
+	var output_file_name		= destination_data_dir + chapterID + ".csv"
+
+	console.log('Converting file ->' + input_file_name);
+	fs = require('fs');
+	fs.readFile(input_file_name, 'utf8', function (err,data) {
+			if (err) {
+				return console.log(err);
+			}
+			
+			var os = require('os');
+			var lines = data.split("\n");
+
+			var output_data = "";	  	
+			var sentence_id = 1;
+		
+			for (var i = 0; i < lines.length; i++) { 
+				if(lines[i] != ""){
+					var convertedLine = convert(lines[i]);
+					var outputLine = "SIN" + chapterID + "-" + generateSentenceID(sentence_id, 4) + "|" + convertedLine + "|" + convertedLine + "\n";
+					output_data += outputLine;	
+
+					sentence_id++; 
+				}    
+			}	
+
+			flushToFile(output_data, output_file_name);
+	});
+}
+
 
 
